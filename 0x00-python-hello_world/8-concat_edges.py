7e9a6d0b46c2f3fa368ba2d1d31406ddd07e6cda
light1414 <dantata49@gmail.com>
2024-01-15 04:46:39 -0800

    pyton project

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interactive, interpreted, object-oriented prog lang that combines remarkable power with clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
